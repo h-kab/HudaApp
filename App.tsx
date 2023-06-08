2a3060d4aac8c23f576999885202971cc01346db
h-kab <136025839+h-kab@users.noreply.github.com>
2023-06-09 00:25:29 +0300

    mm

--- App.tsx
+++ App.tsx
@@ -4,46 +4,7 @@ import MainNavigation from "./route/nav";
 
 
 
-const App = () => { //function component//
-
-  // const data = [
-  //   {
-  //     name: 'A',
-  //     SMS: '19.34569',
-
-  //   },
-  //   {
-  //     name: 'B',
-  //     SMS: '23.63456789',
-
-  //   },
-  //   {
-  //     name: 'C',
-  //     SMS: '21.10345678',
-  //   },
-  //   {
-  //     name: 'D',
-  //     SMS: '28.89876'
-
-  //   },
-  //   {
-  //     name: 'E',
-  //     SMS: '24.8',
-
-  //   },
-  //   {
-  //     name: 'F',
-  //     SMS: '6.7NNHGGEU'
-
-  //   }
-  // ]
-
-  // const render = () => {
-  //   return data.map(CData => {
-  //     return <ContactItem name={CData.name} SMS={CData.SMS} pic={CData.pic} />
-  //   })
-  // }
-
+const App = () => { 
   return (
     <View style={styles.container} >
       <MainNavigation />
